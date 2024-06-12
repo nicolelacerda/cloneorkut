85f29d923a8b7e9b76b95c8c3ff62061ebbe9d84
nicolelacerda <nicole.lacerda05@gmail.com>
2024-06-12 08:14:16 -0300

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/euodeioacordarcedo")
-def index():
+def euodeioacordarcedo():
   return render_template("euodeioacordarcedo.html")
 
 
